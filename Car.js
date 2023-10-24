e45c8027401f49d8ffb8395a777bb90620edec75
zayan <zayan.m.a@outlook.com>
2023-10-24 19:13:37 +0100

    Neural network
    Created  a neural network with three levels, currently the node values are randomized so it's not intelligent. adapted visualizer from online (Not coded by myself).

--- Car.js
+++ Car.js
@@ -12,28 +12,49 @@ class Car{
         this.angle=0;
         this.damaged=false;
 
+        this.useBrain=controlType=="AI";
+
         if(controlType!= "DUMMY"){
             this.sensor=new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
         this.controls=new Controls(controlType);
 
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         if(!this.damaged){
             this.#move();
             this.polygon=this.#createPolygon();
-            this.damaged=this.#assessDamage(roadBorders);
+            this.damaged=this.#assessDamage(roadBorders,traffic);
         }
         if(this.sensor){
-            this.sensor.update(roadBorders);
+            this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
         }
 
     }
 
-    #assessDamage(roadBorders){
+    #assessDamage(roadBorders,traffic){
         for(let i=0;i<roadBorders.length;i++){
             if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
                 return true;
             }
         }
@@ -102,11 +123,11 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(canvasContext){
+    draw(canvasContext,color){
         if(this.damaged){
             canvasContext.fillStyle="gray";
         }else{
-            canvasContext.fillStyle="black";
+            canvasContext.fillStyle=color;
         }
         canvasContext.beginPath();
         canvasContext.moveTo(this.polygon[0].x, this.polygon[0].y);
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const canvas=document.getElementById("myCanvas");
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=250;
 
-canvas.width=200;
-
-const canvasContext = canvas.getContext("2d");
-const road=new Road(canvas.width/2, canvas.width*0.9);
-const car =new Car(road.getLaneCenter(1),100,30,50,"KEYS");
+const carCTX = carCanvas.getContext("2d");
+const networkCTX = networkCanvas.getContext("2d");
+const road=new Road(carCanvas.width/2, carCanvas.width*0.9);
+const car =new Car(road.getLaneCenter(1),100,30,50,"AI");
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY", 2)
 ];
@@ -18,18 +20,20 @@ function animate(){
     }
     car.update(road.borders,traffic);
 
-    canvas.height=window.innerHeight;
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
 
-    canvasContext.save();
-    canvasContext.translate(0,-car.y+canvas.height*0.7);
+    carCTX.save();
+    carCTX.translate(0,-car.y+carCanvas.height*0.7);
 
-    road.draw(canvasContext);
+    road.draw(carCTX);
     for(let i=0;i<traffic.length;i++){
-        traffic[i].draw(canvasContext);
+        traffic[i].draw(carCTX, "yellow");
     }
-    car.draw(canvasContext);
+    car.draw(carCTX, "green");
 
-    canvasContext.restore();
+    carCTX.restore();
+
+    Visualizer.drawNetwork(networkCTX,car.brain);
     requestAnimationFrame(animate); //Calls animate many times to gives animation effect
-
 }
